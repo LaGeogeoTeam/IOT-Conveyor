d8bbed80e01a150b3cdd1426557884356149d76f
greglmx0 <113339984+greglmx0@users.noreply.github.com>
2024-10-04 16:58:28 +0200

    feat: Add MFRC522 RFID reader support

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
 
-#include <M5Stack.h>
 #include "grbl/Module_GRBL_13.2.h"
 #include "rfid/MFRC522_I2C.h"
 #include <stdio.h>
@@ -33,117 +32,55 @@ TaskHandle_t Task2;
 Module_GRBL _GRBL_A = Module_GRBL(STEPMOTOR_I2C_ADDR_1);
 Module_GRBL _GRBL_B = Module_GRBL(STEPMOTOR_I2C_ADDR_2);
 MFRC522 _MFRC522 = MFRC522(MFRC522_I2C_ADDR);
-
-// GOPLUS2
-
-GoPlus2 goPlus;
-
-#define X_LOCAL 40
-#define Y_LOCAL 30
-
-#define X_OFFSET 160
-#define Y_OFFSET 23
-#define rrmt_item32_timemout_us 9500
-
-int _hub1, hub1 = 0;
-rmt_item32_t signals[1024];
-
-size_t received = 0;
-
-int flag = 0;
-int num = 0;
-
-void doTask()
-{
-  if (num == 4)
-  {
-    num = 0;
-  }
-  else
-  {
-    num++;
-  }
-  Serial.println(num);
-}
-
 void setup()
 {
-  Serial.begin(115200);
-  M5.begin();
-  M5.Power.begin();
 
-  // GRBL
-  Wire.begin(21, 22);
-  _GRBL_A.Init(&Wire);
-  _GRBL_B.Init(&Wire);
-  Serial.begin(115200);
-  M5.Lcd.setTextColor(WHITE, BLACK);
-  M5.Lcd.setTextSize(2);
-  M5.lcd.setBrightness(100);
-  M5.Lcd.setCursor(80, 10);
-  M5.Lcd.println("Control motor");
-  M5.Lcd.setCursor(0, 80);
-  M5.Lcd.println("Stopped");
-  M5.Lcd.setCursor(10, 200);
-  M5.Lcd.println("Forward");
-  _GRBL_A.setMode("distance");
-  M5.Lcd.setCursor(140, 200);
-  M5.Lcd.println("Lock");
-  M5.Lcd.setCursor(220, 200);
-  M5.Lcd.println("Backward");
-  _GRBL_B.setMode("distance");
-
-  _MFRC522.PCD_Init(); // Init _MFRC522
-
-  // GOPLUS2
-  goPlus.begin();
-
-  goPlus.Servo_write_angle(SERVO_NUM0, 0);
-}
-
-void Servo(int angle)
-{
-  Serial.print(angle);
-  goPlus.Servo_write_plusewidth(SERVO_NUM0_PW, angle);
-}
-
-void stepMotor()
-{
-  _GRBL_A.setMotor(50000, 50000, 50000, 200);
-  _GRBL_A.setMotor(0, 0, 0, 0);
-}
-
-void rfidReader()
-{
-  unordered_map<string, string> uuidTag;
-
-  uuidTag["ddecc4df"] = "500";  // yellow
-  uuidTag["2dedc4df"] = "1000"; // green
-  uuidTag["dd32d4df"] = "1500"; // red
-  uuidTag["9dece8df"] = "2000"; // blue
-  String uidString = "";
-  for (byte i = 0; i < _MFRC522.uid.size;
-       i++)
-  { // Output the stored UID data
-    uidString += String(_MFRC522.uid.uidByte[i] < 0x10 ? "0" : "");
-    uidString += String(_MFRC522.uid.uidByte[i], HEX);
-  }
-
-  if (!_MFRC522.PICC_IsNewCardPresent() ||
-      !_MFRC522.PICC_ReadCardSerial())
-  {
-    if (uuidTag.find(uidString.c_str()) != uuidTag.end())
-    {
-      Serial.printf("%s\n", uidString);
-      Servo(stoi(uuidTag.at(uidString.c_str())));
-    }
-  }
-  M5.update();
 }
 
 void loop()
 {
-  stepMotor();
-  rfidReader();
+  // If Button A was pressed,stepmotor will rotate back and forth at a time
+  // 如果按下按钮 A，步进电机将一次来回旋转
+  if (M5.BtnA.wasPressed())
+  { // A button
+    Serial.print(_GRBL_A.readStatus());
+    _GRBL_A.setMotor(5, 5, 5, 200);
+    _GRBL_B.setMotor(5, 5, 5, 200);
+    _GRBL_A.setMotor(0, 0, 0, 200);
+    _GRBL_B.setMotor(0, 0, 0, 200);
+  }
+
+  if (M5.BtnB.wasPressed())
+  {
+    // USE Gcode
+    _GRBL_A.sendGcode("G1 X5Y5Z5 F200");
+    _GRBL_B.sendGcode("G1 X5Y5Z5 F200");
+    _GRBL_A.sendGcode("G1 X0Y0Z0 F200");
+    _GRBL_B.sendGcode("G1 X0Y0Z0 F200");
+  }
+
+  if (M5.BtnC.wasReleased())
+  {
+    _GRBL_A.unLock();
+    _GRBL_B.unLock();
+  }
+
+  M5.Lcd.setCursor(40, 47);
+  if (!_MFRC522.PICC_IsNewCardPresent() ||
+      !_MFRC522.PICC_ReadCardSerial()) {
+      delay(200);
+        M5.Lcd.clearDisplay ();
+        M5.Lcd.println("Press Btn A/B FOR Motor");
+        M5.Lcd.println("Please put the card\n\nUID:");
+        M5.Lcd.println("");
+      return;
+  }
+  M5.Lcd.fillRect(42, 47, 320, 20, BLACK);
+  M5.Lcd.clearDisplay ();
+  for (byte i = 0; i < _MFRC522.uid.size;
+        i++) {  // Output the stored UID data
+      M5.Lcd.print(_MFRC522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+      M5.Lcd.print(_MFRC522.uid.uidByte[i], HEX);
+  }
   M5.update();
 }
