5c0753fafc928d10c613da194498c18cc6b80be4
Elouan Da Costa Peixoto <elouandacostapeixoto@gmail.com>
2024-11-14 17:30:03 +0100

    feat: try to make the servo motor turn according to the rfid tag

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,38 @@
 
 #include <M5Stack.h>
 #include "grbl/Module_GRBL_13.2.h"
+#include "rfid/MFRC522_I2C.h"
 #include <stdio.h>
 #include "GoPlus2.h"
 #include <driver/rmt.h>
 #include <math.h>
+#undef min
+#undef max
+#include <thread>
+#include <chrono>
+#include <unordered_map>
+using namespace std;
 
 /*
  * The I2C address of GRBL 13.2  Module is 0x70 by default.
  * You could use the DIP Switch for modify I2C address to 0x71
  */
 
+#define min(a, b) ((a) < (b) ? (a) : (b))
+#define max(a, b) ((a) > (b) ? (a) : (b))
+
+TaskHandle_t Task1;
+TaskHandle_t Task2;
+
 // GRBL
 
 #define STEPMOTOR_I2C_ADDR_1 0x70
 #define STEPMOTOR_I2C_ADDR_2 0x71
+#define MFRC522_I2C_ADDR 0x28
 
 Module_GRBL _GRBL_A = Module_GRBL(STEPMOTOR_I2C_ADDR_1);
 Module_GRBL _GRBL_B = Module_GRBL(STEPMOTOR_I2C_ADDR_2);
+MFRC522 _MFRC522 = MFRC522(MFRC522_I2C_ADDR);
 
 // GOPLUS2
 
@@ -53,6 +68,7 @@ void doTask()
 
 void setup()
 {
+  Serial.begin(115200);
   M5.begin();
   M5.Power.begin();
 
@@ -77,29 +93,25 @@ void setup()
   M5.Lcd.println("Backward");
   _GRBL_B.setMode("distance");
 
+  _MFRC522.PCD_Init(); // Init _MFRC522
+
   // GOPLUS2
   goPlus.begin();
+
+  goPlus.Servo_write_angle(SERVO_NUM0, 0);
 }
 
-void Servo()
+void Servo(int angle)
 {
-  goPlus.Servo_write_angle(SERVO_NUM0, 0);
-  goPlus.Servo_write_angle(SERVO_NUM1, 0);
-  goPlus.Servo_write_angle(SERVO_NUM2, 0);
-  goPlus.Servo_write_angle(SERVO_NUM3, 0);
+  Serial.print(angle);
   delay(1000);
-  goPlus.Servo_write_plusewidth(SERVO_NUM0_PW, 2500);
-  goPlus.Servo_write_plusewidth(SERVO_NUM1_PW, 2500);
-  goPlus.Servo_write_plusewidth(SERVO_NUM2_PW, 2500);
-  goPlus.Servo_write_plusewidth(SERVO_NUM3_PW, 2500);
-  delay(1000);
+  goPlus.Servo_write_plusewidth(SERVO_NUM0_PW, angle);
 }
 
 void stepMotor()
 {
   bool isLooping = false;
   int loopDirection = 0;
-  M5.update();
 
   if (M5.BtnA.wasPressed())
   {
@@ -136,8 +148,42 @@ void stepMotor()
   }
 }
 
+void rfidReader()
+{
+  unordered_map<string, string> uuidTag;
+
+  uuidTag["9d32d4df"] = "1000";
+  uuidTag["3d9ed3df"] = "2000";
+  uuidTag["cd3bd4df"] = "3000";
+  uuidTag["8d3bd4df"] = "4000";
+  String uidString = "";
+  for (byte i = 0; i < _MFRC522.uid.size;
+       i++)
+  { // Output the stored UID data
+    uidString += String(_MFRC522.uid.uidByte[i] < 0x10 ? "0" : "");
+    uidString += String(_MFRC522.uid.uidByte[i], HEX);
+  }
+
+  if (!_MFRC522.PICC_IsNewCardPresent() ||
+      !_MFRC522.PICC_ReadCardSerial())
+  {
+
+    if (uuidTag.find(uidString.c_str()) != uuidTag.end())
+    {
+      Serial.print(uidString);
+      Servo(stoi(uuidTag.at(uidString.c_str())));
+    }
+  }
+  else
+  {
+    goPlus.Servo_write_angle(SERVO_NUM0, 0);
+  }
+  M5.update();
+}
+
 void loop()
 {
   stepMotor();
-  Servo();
+  rfidReader();
+  M5.update();
 }
