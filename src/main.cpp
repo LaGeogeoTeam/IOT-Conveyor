cffbeacbad67226149b5bdd2b4eeabdbb8f4dc9f
Elouan Da Costa Peixoto <elouandacostapeixoto@gmail.com>
2024-11-14 10:43:51 +0100

    feat: make the servo motor turn in same time than the step motor
    At least theoretically because I can't power the two module at the same time

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,16 @@
 #include <M5Stack.h>
 #include "grbl/Module_GRBL_13.2.h"
 #include <stdio.h>
+#include "GoPlus2.h"
+#include <driver/rmt.h>
+#include <math.h>
 
 /*
  * The I2C address of GRBL 13.2  Module is 0x70 by default.
  * You could use the DIP Switch for modify I2C address to 0x71
  */
+
+// GRBL
 
 #define STEPMOTOR_I2C_ADDR_1 0x70
 #define STEPMOTOR_I2C_ADDR_2 0x71
@@ -15,10 +20,45 @@ Module_GRBL _GRBL_A = Module_GRBL(STEPMOTOR_I2C_ADDR_1);
 Module_GRBL _GRBL_B = Module_GRBL(STEPMOTOR_I2C_ADDR_2);
 
 String motorStatus = "Stopped";
+
+// GOPLUS2
+
+GoPlus2 goPlus;
+
+#define X_LOCAL 40
+#define Y_LOCAL 30
+
+#define X_OFFSET 160
+#define Y_OFFSET 23
+#define rrmt_item32_timemout_us 9500
+
+int _hub1, hub1 = 0;
+rmt_item32_t signals[1024];
+
+size_t received = 0;
+
+int flag = 0;
+int num = 0;
+
+void doTask()
+{
+  if (num == 4)
+  {
+    num = 0;
+  }
+  else
+  {
+    num++;
+  }
+  Serial.println(num);
+}
+
 void setup()
 {
   M5.begin();
   M5.Power.begin();
+
+  // GRBL
   Wire.begin(21, 22);
   _GRBL_A.Init(&Wire);
   _GRBL_B.Init(&Wire);
@@ -38,10 +78,28 @@ void setup()
   M5.Lcd.setCursor(220, 200);
   M5.Lcd.println("Backward");
   _GRBL_B.setMode("distance");
+
+  // GOPLUS2
+  goPlus.begin();
+}
+
+void Servo()
+{
+  goPlus.Servo_write_angle(SERVO_NUM0, 0);
+  goPlus.Servo_write_angle(SERVO_NUM1, 0);
+  goPlus.Servo_write_angle(SERVO_NUM2, 0);
+  goPlus.Servo_write_angle(SERVO_NUM3, 0);
+  delay(1000);
+  goPlus.Servo_write_plusewidth(SERVO_NUM0_PW, 2500);
+  goPlus.Servo_write_plusewidth(SERVO_NUM1_PW, 2500);
+  goPlus.Servo_write_plusewidth(SERVO_NUM2_PW, 2500);
+  goPlus.Servo_write_plusewidth(SERVO_NUM3_PW, 2500);
+  delay(1000);
 }
 
 void loop()
 {
+  Servo();
   bool isLooping = false;
   int loopDirection = 0;
   M5.update();
