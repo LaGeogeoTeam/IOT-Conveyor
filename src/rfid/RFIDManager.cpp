9191d7c87d2e25bf71c110ca26ec88198784d809
greglmx0 <113339984+greglmx0@users.noreply.github.com>
2024-11-22 14:23:03 +0100

    feat: Implement RFIDManager for MFRC522 card reading functionality

---
+++ src/rfid/RFIDManager.cpp
@@ -0,0 +1,27 @@
+#include "RFIDManager.h"
+
+#define MFRC522_I2C_ADDR 0x28
+MFRC522 _MFRC522 = MFRC522(MFRC522_I2C_ADDR);
+
+void initMFRC522(){
+    _MFRC522.PCD_Init();
+}
+
+String getCardUID() {
+    // Vérifie si une nouvelle carte est présente
+    if (!_MFRC522.PICC_IsNewCardPresent() || !_MFRC522.PICC_ReadCardSerial()) {
+        return ""; // Pas de carte détectée
+    }
+
+    // Construire l'UID sous forme de chaîne de caractères
+    String uid = "";
+    for (byte i = 0; i < _MFRC522.uid.size; i++) {
+        if (_MFRC522.uid.uidByte[i] < 0x10) {
+            uid += "0"; // Ajout d'un zéro pour les valeurs hexadécimales < 10
+        }
+        uid += String(_MFRC522.uid.uidByte[i], HEX); // Conversion en hexadécimal
+    }
+
+    uid.toUpperCase(); // Convertir la chaîne en majuscules (modifie en place)
+    return uid; // Retourner l'UID
+}
