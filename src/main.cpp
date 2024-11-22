1ae847ec9170a2f99290eb412ba0d384cc3dfa25
Geoffrey Guillouët <geoffrey.guillouet@epitech.eu>
2024-11-22 14:37:23 +0100

    fix: merge conflict and clean branch

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,7 @@
 #include "wifi/WifiManager.h"
 #include "api/APIClient.h"
 #include "json/JsonParser.h"
-#include "rfid/reader/MFRC522_I2C.h"
-
-#define MFRC522_I2C_ADDR 0x28
+#include "rfid/RFIDManager.h"
 
 const char *ssid = "La Geogeo Team";
 const char *password = "LaGeogeoTeam";
@@ -12,28 +10,6 @@ const char *baseURL = "http://146.59.232.167:8081/api/index.php/";
 const char *token = "c3iwyi9dcQ2s";
 
 APIClient apiClient(baseURL, token);
-
-MFRC522 _MFRC522 = MFRC522(MFRC522_I2C_ADDR);
-
-String getCardUID() {
-    // Vérifie si une nouvelle carte est présente
-    if (!_MFRC522.PICC_IsNewCardPresent() || !_MFRC522.PICC_ReadCardSerial()) {
-        return ""; // Pas de carte détectée
-    }
-
-    // Construire l'UID sous forme de chaîne de caractères
-    String uid = "";
-    for (byte i = 0; i < _MFRC522.uid.size; i++) {
-        if (_MFRC522.uid.uidByte[i] < 0x10) {
-            uid += "0"; // Ajout d'un zéro pour les valeurs hexadécimales < 10
-        }
-        uid += String(_MFRC522.uid.uidByte[i], HEX); // Conversion en hexadécimal
-    }
-
-    uid.toUpperCase(); // Convertir la chaîne en majuscules (modifie en place)
-    return uid; // Retourner l'UID
-}
-
 
 
 void setup() {
@@ -48,8 +24,7 @@ void setup() {
   M5.Lcd.setTextSize(2);
   M5.Lcd.setBrightness(100);
   M5.Lcd.setCursor(20, 20);
-  // M5.Lcd.println("MFRC522 Test");
-  _MFRC522.PCD_Init();  // Init _MFRC522
+  initMFRC522();  // Init _MFRC522
   M5.Lcd.println("Please put the card\n\nUID:");
 }
 
@@ -67,7 +42,6 @@ void loop() {
       M5.Lcd.clear();
       M5.Lcd.setCursor(20, 20);
       M5.Lcd.println("Warehouse Id: " + warehouseId);        
-      delay(200); // Pause pour éviter la lecture continue de la même carte
-      
+      delay(200); // Pause pour éviter la lecture continue de la même carte 
   }
 }
