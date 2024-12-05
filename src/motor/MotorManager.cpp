63e1a17fdd4881b4db03b1ee3116880bc0a4812b
Geoffrey Guillouët <geoffrey.guillouet@epitech.eu>
2024-12-05 10:14:41 +0100

    refacto: remove all serial print

--- src/motor/MotorManager.cpp
+++ src/motor/MotorManager.cpp
@@ -16,8 +16,6 @@ void MotorManager::initMotor()
 
 void MotorManager::servoMotor(int angle)
 {
-  Serial.println("Servo-motor move to ");
-  Serial.print(angle);
   //this->goPlus.Servo_write_angle(SERVO_NUM0, angle);//entre 0° et 180°
   this->goPlus.Servo_write_plusewidth(SERVO_NUM0_PW, angle); //entre 500 et 2500 micro seconde
 }
@@ -30,15 +28,8 @@ void MotorManager::stepMotor()
 
 void MotorManager::defineAngleForServoMotor(const int id)
 {
-  Serial.print("id : ");
-  Serial.println(id);
-  Serial.println("define angle");
   for(int cpt = 0; cpt < 3; cpt++){
-    Serial.print("warehouse id : ");
-    Serial.println(warehouseIdArray[cpt][0]);
     if(id == warehouseIdArray[cpt][0]){
-      Serial.print("angle : ");
-      Serial.println(warehouseIdArray[cpt][1]);
       this->servoMotor(warehouseIdArray[cpt][1]);
     }
   }
