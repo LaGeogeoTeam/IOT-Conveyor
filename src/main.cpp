cf028cb0b7a512fd3ed8b245d749ed9ffd396c56
Elouan Da Costa Peixoto <elouandacostapeixoto@gmail.com>
2025-01-16 14:18:38 +0100

    feat: add M5BurnerNVS library and initialize NVS in setup

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include "motor/MotorManager.h"
 #include <stdio.h>
 #include <math.h>
+#include <M5BurnerNVS.h>
 
 using namespace std;
 
@@ -13,31 +14,31 @@ using namespace std;
 const char *ssid = "La Geogeo Team";
 const char *password = "LaGeogeoTeam";
 
-//DOLIBARR
+// DOLIBARR
 const char *baseURL = "http://146.59.232.167:8081/api/index.php/";
 const char *token = "c3iwyi9dcQ2s";
-
 
 APIClient apiClient(baseURL, token);
 MotorManager motorManager;
 RFIDManager rfidManager;
+M5BurnerNVS burner_nvs;
 
 void setup()
 {
   Serial.begin(115200);
   M5.begin();
   M5.Power.begin();
-  //Init des moteurs servo et step
+  // Init des moteurs servo et step
   motorManager.initMotor();
 
   M5.Lcd.setTextColor(WHITE, BLACK);
   M5.Lcd.setTextSize(2);
   M5.Lcd.setBrightness(100);
-  
+
   auto cfg = M5.config();
   cfg.external_spk = true;
 
-  //Connection to wifi
+  // Connection to wifi
   connectToWiFi(ssid, password);
 
   M5.Lcd.setTextColor(WHITE, BLACK);
@@ -45,17 +46,23 @@ void setup()
   M5.Lcd.setBrightness(100);
   M5.Lcd.setCursor(20, 20);
   // Init _MFRC522
-  rfidManager.initMFRC522(); 
+  rfidManager.initMFRC522();
   M5.Lcd.println("Please put the card\n\nUID:");
+
+  burner_nvs.init();
+  burner_nvs.listen();
+
+  burner_nvs.set("key", "prout");
 }
 
 void loop()
 {
   M5.update();
-  //motorManager.stepMotor(); 
+  // motorManager.stepMotor();
 
   reconnectWiFi(ssid, password);
 
+  Serial.println(burner_nvs.get("key"));
   String uid = rfidManager.getCardUID();
   if (uid != "")
   { // Si une carte est détectée
@@ -68,7 +75,7 @@ void loop()
     M5.Lcd.clear();
     M5.Lcd.setCursor(20, 20);
     M5.Lcd.println("Warehouse Id: " + warehouseId);
-    
+
     motorManager.defineAngleForServoMotor(warehouseId.toInt());
 
     delay(1000); // Pause pour éviter la lecture continue de la même carte
