1fa1889303d2eac25f9f8bda7ef6bc549bc51768
Geoffrey Guillouët <geoffrey.guillouet@epitech.eu>
2025-01-24 15:24:58 +0100

    feat: step motor ok

--- src/main.cpp
+++ src/main.cpp
@@ -27,6 +27,8 @@ WebConfigServer webserver;
 String ssid = "";
 String password = "";
 
+int cpt = 0;
+
 void setup()
 {
   auto cfg = M5.config();
@@ -34,51 +36,13 @@ void setup()
   Serial.begin(115200);
   M5.begin(cfg);
   M5.Power.begin();
-
-  // WIFI and WebServer
-  wifiManager.loadWiFiCredentials(ssid, password);
-
-  if (ssid == "" || password == "")
-  {
-    Serial.println("Pas de WiFi config - Lancement AP Mode (premiere mise en place)...");
-    wifiManager.startAPMode();
-  }
-  else
-  {
-    wifiManager.connectToWiFi(ssid, password);
-    wifiManager.waitForWiFi();
-
-    if (!isWiFiConnected)
-    {
-      wifiManager.startAPMode();
-    }
-  }
-  webserver.begin();
+  
   // Init des moteurs servo et step
   motorManager.initMotor();
-
-  M5.Lcd.setTextColor(WHITE, BLACK);
-  M5.Lcd.setTextSize(2);
-  M5.Lcd.setBrightness(100);
-
-  // Init RFID
-  rfidManager.initMFRC522();
-  preferences.begin("rfid", false);
-  // preferences.putBool("rwMode", false);
 }
 
 void loop()
 {
-
-  // get the rfid mode from nvs
-  bool rfidMode = preferences.getBool("rwMode");
-  M5.Lcd.setCursor(20, 150);
-  M5.Lcd.println("RFID current mode: ");
-  M5.Lcd.setCursor(240, 150);
-  M5.Lcd.println(rfidMode);
   M5.update();
   motorManager.startStepMotor();
-  rfidManager.rfidConveyor(baseURL, token);
-
-  wifiManager.reconnectWiFi(ssid, password);
 }
